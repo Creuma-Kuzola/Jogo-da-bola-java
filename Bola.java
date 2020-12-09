eb5b0a1a0b83c4a5f419062118a9320739262307
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-12-09 23:20:04 +0100

    Bug fix one

---
+++ Bola.java
@@ -0,0 +1,42 @@
+public class Bola {
+    private int posX, posY;
+    private int width, height;
+
+    public Bola(int posX, int posY, int width, int height) {
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+    }
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
--- JogoBola.java
+++ JogoBola.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package JogoBola;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
